826d25d0efde105769e6693ced93637ae834b3d3
ejjarder <suaveh@gmail.com>
2014-06-13 22:35:24 +0800

    cachematrix implementation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+###############################################################################
+#
+#   cachematrix.R
+#   Author: Eugene Jarder
+#
+#   This file contains functions related to caching the inverse of a matrix. 
+#
+#   Requirement for R Programming course at Coursera
+#
+###############################################################################
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# makeCacheMatrix()
+# Creates a CacheMatrix, a matrix whose inverse is cacheable.
+# The user can pass a matrix as a parameter to initialize its value
+makeCacheMatrix <- function(thismatrix = matrix())
+{
+    thisinverse <- NULL
+    
+    # set the inverse of the CacheMatrix
+    set <- function(thatmatrix)
+    {
+        thismatrix <<- thatmatrix
+        thisinverse <<- NULL
+    }
+    
+    # get the inverse of the CacheMatrix
+    get <- function() thismatrix
+    
+    # set the inverse of the CacheMatrix
+    setinverse <- function(thatinverse) thisinverse <<- thatinverse
+    
+    # get the inverse of the CacheMatrix
+    getinverse <- function() thisinverse
+    
+    # make the functions accessible as a list
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# cacheSolve()
+# Computes the inverse of the matrix. Uses the solve() built in function
+# Users can pass solve()'s optional parameters as well to modify the result.
+cacheSolve <- function(cachedmatrix, ...)
+{
+    thisinverse <- cachedmatrix$getinverse()
+    if (!is.null(thisinverse))
+    {
+        message("Inverse is set! Retrieving cached inverse")
+        return(thisinverse)
+    }
+    thismatrix <- cachedmatrix$get()
+    thisinverse <- solve(thismatrix, ...)
+    cachedmatrix$setinverse(thisinverse)
+    thisinverse
 }
